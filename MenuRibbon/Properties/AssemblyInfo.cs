60bf094797792865bd119a363ad4887e3df52c63
superlloyd <ld@galador.net>
2015-01-02 01:47:51 +0930

    starting the NuGet package

--- MenuRibbon/Properties/AssemblyInfo.cs
+++ MenuRibbon/Properties/AssemblyInfo.cs
@@ -32,21 +32,6 @@ using System.Windows.Markup;
 //[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
 
 
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.5.0.0")]
-[assembly: AssemblyFileVersion("0.5.0.0")]
-
-
 [assembly: XmlnsPrefix("http://schemas.menuribbon.net/xaml", "mr")]
 [assembly: XmlnsDefinition("http://schemas.menuribbon.net/xaml", "MenuRibbon.WPF")]
 [assembly: XmlnsDefinition("http://schemas.menuribbon.net/xaml", "MenuRibbon.WPF.Markup")]
