03e1ef6676495cd1acbc821bf1b0de46977f732b
superlloyd <ld@galador.net>
2018-10-30 20:26:35 +1000

    CommandTool: simpler, + (new) GetCommandContext()

--- MenuRibbon/CommandTool.cs
+++ MenuRibbon/CommandTool.cs
@@ -8,64 +8,80 @@ using System.Windows.Input;
 
 namespace MenuRibbon.WPF
 {
-	public static class CommandTool
-	{
-		static IInputElement GetTarget(this ICommandSource commandSource)
-		{
-			var target = commandSource.CommandTarget;
-			if (target == null && Keyboard.FocusedElement == null)
-				target = commandSource as IInputElement;
-			return target;
-		}
+    public static class CommandTool
+    {
+        public static object GetCommandContext(this ICommandSource commandSource)
+        {
+            ICommand command = commandSource.Command;
+            if (command == null)
+                return false;
 
-		public static bool CanExecuteCommand(this ICommandSource commandSource)
-		{
-			ICommand command = commandSource.Command;
-			if (command == null)
-				return false;
-			object parameter = commandSource.CommandParameter;
-			IInputElement target = commandSource.GetTarget();
-			return CanExecuteCommand(command, parameter, target);
-		}
-		public static bool CanExecuteCommand(ICommand command, object parameter, IInputElement target)
-		{
-			if (command == null)
-				return false;
-			RoutedCommand routed = command as RoutedCommand;
-			if (routed != null)
-				return routed.CanExecute(parameter, target);
-			return command.CanExecute(parameter);
-		}
+            if (command is RoutedCommand routed)
+            {
+                if (commandSource.GetTarget() is FrameworkElement fe)
+                    return fe.DataContext;
+            }
+            else
+            {
+                if (commandSource is FrameworkElement fe)
+                    return fe.DataContext;
+            }
+            return null;
+        }
 
-		public static bool ExecuteCommand(this ICommandSource commandSource)
-		{
-			ICommand command = commandSource.Command;
-			if (command == null)
-				return false;
-			object parameter = commandSource.CommandParameter;
-			IInputElement target = commandSource.GetTarget();
-			return ExecuteCommand(command, parameter, target);
-		}
-		public static bool ExecuteCommand(ICommand command, object parameter, IInputElement target)
-		{
-			if (command == null)
-				return false;
+        static IInputElement GetTarget(this ICommandSource commandSource)
+        {
+            var target = commandSource.CommandTarget;
+            if (target == null && Keyboard.FocusedElement == null)
+                target = commandSource as IInputElement;
+            return target;
+        }
 
-			RoutedCommand routed = command as RoutedCommand;
-			if (routed != null)
-			{
-				if (routed.CanExecute(parameter, target))
-				{
-					routed.Execute(parameter, target);
-					return true;
-				}
-			}
-			else if (command.CanExecute(parameter))
-			{
-				command.Execute(parameter);
-				return true;
-			}
-			return false;
-		}
-	}
+        public static bool CanExecuteCommand(this ICommandSource commandSource)
+        {
+            ICommand command = commandSource.Command;
+            if (command == null)
+                return false;
+
+            object parameter = commandSource.CommandParameter;
+
+            if (command is RoutedCommand routed)
+            {
+                var target = commandSource.GetTarget();
+                return target != null && routed.CanExecute(parameter, target);
+            }
+            else
+            {
+                return command.CanExecute(parameter);
+            }
+        }
+
+        public static bool ExecuteCommand(this ICommandSource commandSource)
+        {
+            ICommand command = commandSource.Command;
+            if (command == null)
+                return false;
+
+            object parameter = commandSource.CommandParameter;
+
+            if (command is RoutedCommand routed)
+            {
+                var target = commandSource.GetTarget();
+                if (target != null && routed.CanExecute(parameter, target))
+                {
+                    routed.Execute(parameter, target);
+                    return true;
+                }
+            }
+            else
+            {
+                if (command.CanExecute(parameter))
+                {
+                    command.Execute(parameter);
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
 }
