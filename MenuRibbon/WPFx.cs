84a05aa76be4628307968d67b005707d71d8cc6b
superlloyd <ld@galador.net>
2021-12-06 20:51:16 +1000

    added ToAsyncEnumerable(CancelToken)

--- MenuRibbon/WPFx.cs
+++ MenuRibbon/WPFx.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Reactive;
 using System.Reactive.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
@@ -232,6 +233,34 @@ namespace MenuRibbon.WPF
 				on => Mouse.AddMouseUpHandler(that, (o, e) => on(e)),
 				on => Mouse.RemoveMouseUpHandler(that, (o, e) => on(e))
 			);
+		}
+
+		#endregion
+
+		#region ToAsyncEnumerable()
+
+		public static IAsyncEnumerable<T> ToAsyncEnumerable<T>(this IObservable<T> observable, CancellationToken token)
+			=> new CancellableObservable<T>(observable, token).ToAsyncEnumerable();
+
+		class CancellableObservable<T> : IObservable<T>
+		{
+			readonly IObservable<T> events;
+			readonly CancellationToken token;
+
+			public CancellableObservable(IObservable<T> events, CancellationToken token)
+			{
+				this.events = events;
+				this.token = token;
+			}
+
+			public IDisposable Subscribe(IObserver<T> observer)
+			{
+				var result = events.Subscribe(observer);
+				token.Register(observer.OnCompleted);
+				if (token.IsCancellationRequested)
+					observer.OnCompleted();
+				return result;
+			}
 		}
 
 		#endregion
